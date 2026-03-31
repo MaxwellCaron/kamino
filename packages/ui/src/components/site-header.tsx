bb27254ebd027fbf3d1ccfa33d6f35d6c97269a2
MaxwellCaron <maxwell.caron1@gmail.com>
2026-03-31 14:11:51 -0700

    feat: added dark/light theme dropdown to header

--- packages/ui/src/components/site-header.tsx
+++ packages/ui/src/components/site-header.tsx
@@ -1,16 +1,20 @@
 import { Separator } from "@workspace/ui/components/separator"
 import { SidebarTrigger } from "@workspace/ui/components/sidebar"
+import { ThemeToggle } from "./theme-toggle"
 
 export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mx-2 h-4 data-vertical:self-auto"
-        />
-        <h1 className="text-base font-medium">Documents</h1>
+      <div className="flex w-full items-center justify-between gap-1 px-4 lg:gap-2 lg:px-6">
+        <div className="flex items-center">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mx-2 h-4 data-vertical:self-auto"
+          />
+          <h1 className="text-base font-medium">Documents</h1>
+        </div>
+        <ThemeToggle />
       </div>
     </header>
   )
