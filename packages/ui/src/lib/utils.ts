33f07bc623eb7c81591a4ade7c162c5d95946a24
MaxwellCaron <maxwell.caron1@gmail.com>
2026-06-23 16:27:47 -0700

    fix: no overload matches this call

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,1 +1,10 @@
-export { cn, type ClassValue } from "cnfast"
+import { cn as cnfast } from "cnfast"
+import type { ClassValue as CnfastClassValue } from "cnfast"
+
+export type ClassValue =
+  | CnfastClassValue
+  | ((...args: Array<never>) => string | undefined)
+
+export function cn(...inputs: Array<ClassValue>) {
+  return cnfast(...(inputs as Array<CnfastClassValue>))
+}
