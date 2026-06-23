9b03944343a01a0232fecf66b6d404a0269dfdde
MaxwellCaron <maxwell.caron1@gmail.com>
2026-06-23 13:48:11 -0700

    chore: bun update

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,10 +1,1 @@
-import { cn as cnfast } from "cnfast"
-import type { ClassValue as CnfastClassValue } from "cnfast"
-
-export type ClassValue =
-  | CnfastClassValue
-  | ((...args: Array<never>) => string | undefined)
-
-export function cn(...inputs: Array<ClassValue>) {
-  return cnfast(...(inputs as Array<CnfastClassValue>))
-}
+export { cn, type ClassValue } from "cnfast"
